632c3fda2ccc86f36c1c2052c8d1947061f9e275
kiwon kwon <funnykyeon@naver.com>
2022-07-05 13:51:26 +0900

    Feat: 회원가입,로그인 기능 추가

---
+++ src/main/java/spring/springtf/domain/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package spring.springtf.domain.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import spring.springtf.domain.entity.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
